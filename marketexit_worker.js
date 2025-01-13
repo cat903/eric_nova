f74948b83ab4715e727cccd510e9ea44a6a900c6
cat903 <75070039+cat903@users.noreply.github.com>
2025-01-13 17:14:15 +0600

    Update marketexit_worker.js

--- marketexit_worker.js
+++ marketexit_worker.js
@@ -12,21 +12,23 @@ async function executeMarketExitAction(data) {
   const openPositions = await getOpenPosition(require('./config.json'));
   console.log(openPositions);
   const status = data.action === 'buy' ? 'short' : 'long';
-  if (openPositions.length === 1) {
+  if (openPositions?.length === 1) {
     const resultofOrder = await marketOrder(data.action,require('./config.json'));
     await sendtoDiscord(`exit ->-> ${data.action} ->-> ${data.symbol}@${data.entryPrice}`);
     await delay(15000);
     const openPositions = await getOpenPosition(require('./config.json'));
-    if (openPositions.length === 0){
+    if (openPositions?.length === 0){
       await sendtoDiscord(`${moment().tz("Asia/Kuala_Lumpur").format('YYYY-MM-DD HH:mm:ss')} ->-> filled exit ->-> ${data.action} ->-> ${data.symbol}@${openPositions[0].AveragePrice}`);
       const orderHistory = await getOrderHistory(require('./config.json'));
       const profitLoss = calculateProfitLoss(orderHistory,status);
       await sendtoDiscord(`${profitLoss?.result} -> RM ${profitLoss?.amount}`);
+      console.log('exit action completed successfully');
     }
     else{
       await sendtoDiscord(`trade rejected exit ->-> ${data.action} ->-> ${data.symbol}@${data.entryPrice}`);
+      console.log('exit action failed trade rejected');
     }
-    return 'exit action completed successfully!';
+    return 'exit attempt completed successfully!';
   }
 }
 
