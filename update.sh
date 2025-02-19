9c9c0dff40db817787ad8cd7cbc1cb56421238c2
cat903 <75070039+cat903@users.noreply.github.com>
2025-02-19 12:05:43 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Clone the GitHub repository
-echo "Cloning repository..."
-git clone https://github.com/cat903/eric_nova.git
+REPO_DIR="eric_nova"
+REPO_URL="https://github.com/cat903/eric_nova.git"
 
-# Changing Directory to the GitHub repository
-cd eric_nova
+if [ -d "$REPO_DIR" ]; then
+    echo "Directory '$REPO_DIR' already exists. Updating repository..."
+    cd "$REPO_DIR" || exit
+    git pull
+else
+    echo "Cloning repository..."
+    git clone "$REPO_URL"
+    cd "$REPO_DIR" || exit
+fi
 
-# Install project dependencies and start with PM2
 echo "Installing project dependencies..."
 npm install --save
+
 echo "Starting application with PM2..."
 pm2 start ecosystem.config.js
