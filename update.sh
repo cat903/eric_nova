1c6c9ae9234ed563ff578ebabb3f1b9e1238ec7c
cat903 <75070039+cat903@users.noreply.github.com>
2025-02-21 01:08:55 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,6 +7,11 @@ rm -rf eric_nova
 git clone https://github.com/cat903/eric_nova.git
 cd eric_nova
 
+# Load nvm into the current shell session
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 echo "Installing PM2 globally..."
 npm install pm2 -g
 
