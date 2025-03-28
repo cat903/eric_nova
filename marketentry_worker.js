76a0f1acfd8ddcd549e33dd194ffc4c765a61c91
cat903 <75070039+cat903@users.noreply.github.com>
2025-03-28 14:16:35 +0600

    Update marketentry_worker.js

--- marketentry_worker.js
+++ marketentry_worker.js
@@ -36,7 +36,9 @@ async function processEntryCompletion(action, symbol, entryPrice, openPositions)
 }
 
 async function executeMarketEntryAction(data) {
-  const openPositions = await checkOpenPositions(data.action, data.symbol, data.entryPrice);
+  const allowedtoEnterMarket = canTrade();
+  if(allowedtoEnterMarket){
+      const openPositions = await checkOpenPositions(data.action, data.symbol, data.entryPrice);
 
   if (!openPositions) return 'Entry action failed: could not get open positions';
 
@@ -53,6 +55,34 @@ async function executeMarketEntryAction(data) {
   }
 
   return 'Entry action not required: position already open';
+  }
+}
+
+const closingTimes = [
+    { day: 1, times: ["12:27", "17:55", "23:25"] }, // Monday
+    { day: 2, times: ["12:27", "17:55", "23:25"] }, // Tuesday
+    { day: 3, times: ["12:27", "17:55", "23:25"] }, // Wednesday
+    { day: 4, times: ["12:25", "17:55", "23:25"] }, // Thursday
+    { day: 5, times: ["12:25", "17:55"] },          // Friday (No night market)
+];
+
+function canTrade() {
+    const now = moment().tz("Asia/Kuala_Lumpur");
+    const currentDay = now.isoWeekday(); // Monday = 1, Sunday = 7
+    const currentTime = now.format('HH:mm');
+
+    const marketDay = closingTimes.find(day => day.day === currentDay);
+    if (!marketDay) return; // No market today (Saturday/Sunday)
+
+    marketDay.times.forEach(closingTime => {
+        const closingMoment = moment.tz(`${now.format('YYYY-MM-DD')} ${closingTime}`, "Asia/Kuala_Lumpur");
+        const diffMinutes = closingMoment.diff(now, 'minutes');
+
+        if (!(diffMinutes >= 0 && diffMinutes <= 5)) {
+            return;
+        }
+        return true;
+    });
 }
 
 executeMarketEntryAction(workerData)
