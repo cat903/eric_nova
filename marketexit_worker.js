20abe37f6624c38cd821f7b6f04db53af0c0dc1a
cat903 <75070039+cat903@users.noreply.github.com>
2025-04-11 14:14:01 +0600

    Update marketexit_worker.js

--- marketexit_worker.js
+++ marketexit_worker.js
@@ -15,12 +15,13 @@ async function logAndNotify(message) {
   await sendtoDiscord(message);
 }
 
-async function checkOpenPositions(action, symbol, entryPrice) {
+async function checkOpenPositions(action, symbol, entryPrice,retryn=3) {
   const openPositions = await getOpenPosition(require('./config.json'));
-  if (openPositions?.length !== 0 && openPositions?.length !== 1) {
-    const errorMessage = `demo nova server timed out, rejected exit ->-> ${action} ->-> ${symbol}@${entryPrice}`;
+  if ((openPositions?.length !== 0 && openPositions?.length !== 1) && retryn > 0) {
+    const errorMessage = `${retryn} demo nova server timed out, rejected exit ->-> ${action} ->-> ${symbol}@${entryPrice}`;
     await logAndNotify(errorMessage);
-    return null;
+    await delay(5000);
+    return checkOpenPositions(action, symbol, entryPrice,--retryn);
   }
   return openPositions;
 }
