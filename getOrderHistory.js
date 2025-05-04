46a639bd724c715504dc00a983c754bd90cd9df5
cat903 <75070039+cat903@users.noreply.github.com>
2025-05-04 14:33:22 +0600

    Update getOrderHistory.js

--- getOrderHistory.js
+++ getOrderHistory.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const fetch = require('node-fetch');
+
 
 const url = `https://${process.env.PLATFORM}.phillipmobile.com/MobileControlService.svc/GetOrderHistory`;
 
