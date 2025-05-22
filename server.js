ae8047d85cb5998fae5fddb70925c6c77b4f7627
= <tahmid00911@gmail.com>
2025-05-22 13:01:33 +0600

    fixing path

--- server.js
+++ server.js
@@ -57,11 +57,11 @@ app.post('/signal', async (req, res) => {
     console.log('Received webhook:', webhookData);
     if (webhookData.type === '-1' || webhookData.type === '1') {
         console.log('Spawning worker for entry action...');
-        await spawnWorker('./marketentry_worker.js', webhookData);
+        await spawnWorker('./scripts/marketentry_worker.js', webhookData);
     }
     if (webhookData.type === '0') {
         console.log('Spawning worker for exit action...');
-        await spawnWorker('./marketexit_worker.js', webhookData);
+        await spawnWorker('./scripts/marketentry_worker.js', webhookData);
     }
 
     res.status(200).json({ message: 'Webhook processed successfully!' });
